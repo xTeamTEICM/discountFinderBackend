7d53f9239d2679cfb1e6b1008ac224e858dfcd91
sotikele <xsotos@hotmail.com>
2017-11-28 20:06:36 +0200

    About #16  better implementation

--- app/Http/Controllers/FindDiscountsController.php
+++ app/Http/Controllers/FindDiscountsController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 
-use App\Discounts;
+use App\CustomClasses\Distance;
+use App\Shop;
 
 
 use Illuminate\Http\Request;
@@ -21,86 +22,38 @@ class findDiscountsController extends Controller
 
             'logPos' => 'required|numeric',
             'latPos' => 'required|numeric',
-            'distanceInMeters' => 'required|numeric',
-            'maxPrice' => 'nullable|numeric',
-            'category' => 'nullable|numeric',
+            //'distanceInMeters' => 'required|numeric',
 
         ]);
 
 
-        return new DiscountsCollection($this->requestedDiscounts(request('distanceInMeters'),request('maxPrice'),request('category')));
+        return new DiscountsCollection($this->requestedDiscounts());
 
     }
 
 
-    public function  requestedDiscounts($maxDistance,$maxPrice,$category){
+    public function  requestedDiscounts(){
 
-        $maxPrice=$this->optimizePrice($maxPrice);
-        $discounts=Discounts::all();
+        $id=auth('api')->user()->id;
+        $discounts=DB::select("call getMatchedDiscounts($id)");
         $requestedDiscounts =collect();
 
 
         foreach ($discounts as $discount){
-            $shopLogPos =DB::table('shops')->where('id',$discount->shopId)->pluck('logPos')->first();
-            $shopLatPos =DB::table('shops')->where('id',$discount->shopId)->pluck('latPos')->first();
-            $distance=  $this->calculateDistanceInMeters(request('latPos'),request('logPos'),$shopLogPos,$shopLatPos);
 
+            $shopLogPos =Shop::query()->where('id',$discount->shopId)->pluck('logPos')->first();
+            $shopLatPos =Shop::query()->where('id',$discount->shopId)->pluck('latPos')->first();
+            $distanceObject= new Distance(request('latPos'),request('logPos'));
+            $distance=  $distanceObject->calculateDistanceInMeters($shopLogPos,$shopLatPos);
+            //TODO find solution to remove this Global variable
+            \App\Http\Resources\Discounts::$distance[]=$distance;
+            $requestedDiscounts->push($discount) ;
 
-            if($category!=null){
-
-               if($distance<=$maxDistance&&$discount->currentPrice<=$maxPrice&&$category==$discount->category){
-                   \App\Http\Resources\Discounts::$distance[]=$distance;
-                   $requestedDiscounts->push($discount) ;
-                }
-              }
-              else{
-
-                  if($distance<=$maxDistance&&$discount->currentPrice<=$maxPrice){
-                      \App\Http\Resources\Discounts::$distance[]=$distance;
-                      $requestedDiscounts->push($discount) ;
-                  }
-
-              }
         }
+
         return $requestedDiscounts;
     }
 
-
-
-//seting big default maxprice if maxprice is null so we can take all prices
-    public function optimizePrice($maxPrice){
-        if($maxPrice==null){
-            $maxPrice=1000000;
-        }
-        return $maxPrice;
-
-
-    }
-
-
-
-    public function calculateDistanceInMeters($userLatPos,$userLogPos,$shopLogPosition,$shopLatPosition){
-        $userLatPos=deg2rad($userLatPos);
-        $userLogPos=deg2rad($userLogPos);
-        $shopLogPosition=deg2rad($shopLogPosition);
-        $shopLatPosition=deg2rad($shopLatPosition);
-
-
-        $radiusOfEarth = 6371000;// Earth's radius in meters.
-        $diffLatitude = abs($userLatPos - $shopLatPosition);
-        $diffLongitude = abs($userLogPos - $shopLogPosition);
-
-
-
-        $a = sin($diffLatitude / 2) * sin($diffLatitude / 2) +
-            cos($userLatPos) * cos($shopLatPosition) *
-            sin($diffLongitude / 2) * sin($diffLongitude / 2);
-        $c = 2 * asin(sqrt($a));
-        $distance = $radiusOfEarth * $c;
-
-        return (int)$distance;
-
-    }
 
 
 }
