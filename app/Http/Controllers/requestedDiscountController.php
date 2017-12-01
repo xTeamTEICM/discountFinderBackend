d2146316945599f0e407687a105aae702937da84
Jordan Kostelidis <JordanKostelidis@JNKSoftware.eu>
2017-12-01 20:24:59 +0200

    Requested Discount Done

--- app/Http/Controllers/requestedDiscountController.php
+++ app/Http/Controllers/requestedDiscountController.php
@@ -5,22 +5,32 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\requestedDiscount;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\UnauthorizedException;
+use Mockery\Exception;
 
 
 class requestedDiscountController extends Controller
 {
-    public function list() {
+    public function list()
+    {
         $userId = Auth::user()->id;
-        return requestedDiscount::all()->where('userId','=',$userId);
+        return requestedDiscount::query()->where('userId', '=', $userId)->get();
     }
 
-    public function get($id) {
-        // Check if id is numeric
+    public function get($id)
+    {
+        $request = new Request();
+        $request['id'] = $id;
+
+        $data = $this->validate($request, [
+            'id' => 'required|numeric'
+        ]);
         $userId = Auth::user()->id;
-        return requestedDiscount::query()->where('userId','=',$userId)->where('id','=',$id)->firstOrFail();
+        return requestedDiscount::query()->where('userId', '=', $userId)->where('id', '=', $data['id'])->first();
     }
 
-    public function post(Request $request) {
+    public function post(Request $request)
+    {
 
         $data = $this->validate($request, [
             'category' => 'required|numeric',
@@ -42,32 +52,48 @@ class requestedDiscountController extends Controller
         return $requestedDiscount;
     }
 
-    public function put($id, Request $request) {
-// Check if id is numeric
-        $userId = Auth::user()->id;
-        $requestedDiscount = requestedDiscount::query()->where('userId','=',$userId)->where('id','=',$id)->firstOrFail();
+    public function put($id, Request $request)
+    {
+        $request['id'] = $id;
 
         $data = $this->validate($request, [
+            'id' => 'required|numeric',
             'category' => 'required|numeric',
             'price' => 'required|numeric',
             'tags' => 'required|string'
         ]);
 
+        $userId = Auth::user()->id;
 
-        $requestedDiscount->category = $data['category'];
-        $requestedDiscount->price = $data['price'];
-        $requestedDiscount->tags = $data['tags'];
+        $requestedDiscount = requestedDiscount::query()->where('userId', '=', $userId)->where('id', '=', $data['id'])->first();
+        if ($requestedDiscount != null) {
+            $requestedDiscount->category = $data['category'];
+            $requestedDiscount->price = $data['price'];
+            $requestedDiscount->tags = $data['tags'];
 
 
-        $requestedDiscount->save();
-        $requestedDiscount->push();
-        return $requestedDiscount;
+            $requestedDiscount->save();
+            $requestedDiscount->push();
+            return $requestedDiscount;
+        } else {
+            return "";
+        }
     }
 
-    public function delete($id) {
-        // Check if id is numeric
+    public function delete($id)
+    {
+        $request = new Request();
+        $request['id'] = $id;
+
+        $data = $this->validate($request, [
+            'id' => 'required|numeric'
+        ]);
+
         $userId = Auth::user()->id;
-        $requestedDiscount = requestedDiscount::query()->where('userId','=',$userId)->findOrFail($id);
-        $requestedDiscount->delete();
+
+        $requestedDiscount = requestedDiscount::query()->where('userId', '=', $userId)->find($data['id']);
+        if ($requestedDiscount != null) {
+            $requestedDiscount->delete();
+        }
     }
 }
---
+++ tests/Feature/requestedDiscountsControllerTest.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class requestedDiscountsControllerTest extends TestCase
+{
+    protected static $Token_Type;
+    protected static $Access_Token;
+    protected static $id;
+
+    public function testAuth()
+    {
+        $response = $this->json('POST', 'api/login', [
+            'username' => 'testing@local.host',
+            'password' => '1234567'
+        ]);
+
+        requestedDiscountsControllerTest::$Token_Type = $response->decodeResponseJson()['token_type'];
+        requestedDiscountsControllerTest::$Access_Token = $response->decodeResponseJson()['access_token'];
+
+        $this->assertNotEquals("", requestedDiscountsControllerTest::$Token_Type);
+        $this->assertNotEquals("", requestedDiscountsControllerTest::$Access_Token);
+    }
+
+    public function testPostNotExisted()
+    {
+        $response = $this->json('POST', 'api/requestedDiscount', [
+            'category' => '1',
+            'price' => '999',
+            'tags' => 'testingAPI'
+        ], [
+            'Authorization' => requestedDiscountsControllerTest::$Token_Type . " " . requestedDiscountsControllerTest::$Access_Token
+        ]);
+
+        requestedDiscountsControllerTest::$id = $response->decodeResponseJson()['id'];
+
+        $response->assertJsonStructure(['userId', 'category', 'price', 'tags', 'id']);
+    }
+
+    public function testList()
+    {
+        $response = $this->json('GET', 'api/requestedDiscount', [], [
+            'Authorization' => requestedDiscountsControllerTest::$Token_Type . " " . requestedDiscountsControllerTest::$Access_Token
+        ]);
+        $response->assertJsonStructure([['userId', 'category', 'price', 'tags', 'id']]);
+    }
+
+    public function testGetExisted()
+    {
+        $response = $this->json('GET', 'api/requestedDiscount/' . requestedDiscountsControllerTest::$id, [], [
+            'Authorization' => requestedDiscountsControllerTest::$Token_Type . " " . requestedDiscountsControllerTest::$Access_Token
+        ]);
+        $response->assertJsonStructure(['id', 'userId', 'category', 'price', 'tags']);
+    }
+
+    public function testGetNotExisted()
+    {
+        $response = $this->json('GET', 'api/requestedDiscount/0', [], [
+            'Authorization' => requestedDiscountsControllerTest::$Token_Type . " " . requestedDiscountsControllerTest::$Access_Token
+        ]);
+        $this->assertEquals("", $response->content());
+    }
+
+    public function testPutExisted()
+    {
+        $response = $this->json('PUT', 'api/requestedDiscount/' . requestedDiscountsControllerTest::$id, [
+            'category' => '1',
+            'price' => '999',
+            'tags' => 'testingAPI2'
+        ], [
+            'Authorization' => requestedDiscountsControllerTest::$Token_Type . " " . requestedDiscountsControllerTest::$Access_Token
+        ]);
+        $response->assertJsonStructure(['id', 'userId', 'category', 'price', 'tags']);
+
+        // Check Response Data from PUT call
+        $this->assertEquals("1", $response->decodeResponseJson()['category']);
+        $this->assertEquals('999', $response->decodeResponseJson()['price']);
+        $this->assertEquals('testingAPI2', $response->decodeResponseJson()['tags']);
+
+        // Check Response Data from a GET call
+        $responseGet = $this->json('GET', 'api/requestedDiscount/' . requestedDiscountsControllerTest::$id, [], []);
+        $this->assertEquals("1", $responseGet->decodeResponseJson()['category']);
+        $this->assertEquals('999', $responseGet->decodeResponseJson()['price']);
+        $this->assertEquals('testingAPI2', $responseGet->decodeResponseJson()['tags']);
+    }
+
+    public function testPutNotExisted()
+    {
+        $response = $this->json('PUT', 'api/requestedDiscount/0', [
+            'category' => '1',
+            'price' => '999',
+            'tags' => 'testingAPI2'
+        ], [
+            'Authorization' => requestedDiscountsControllerTest::$Token_Type . " " . requestedDiscountsControllerTest::$Access_Token
+        ]);
+        $this->assertEquals("", $response->content());
+    }
+
+
+    public function testDeleteNotExisted()
+    {
+        $responseTrue = $this->json('DELETE', "api/requestedDiscount/" . requestedDiscountsControllerTest::$id,
+            [],
+            [
+                'Authorization' => requestedDiscountsControllerTest::$Token_Type . " " . requestedDiscountsControllerTest::$Access_Token
+            ]);
+
+        $this->assertEquals(200, $responseTrue->getStatusCode());
+    }
+}
