1d2b716829f925d23a62a16b2e3ad4d4d50aad44
Jordan Kostelidis <JordanKostelidis@JNKSoftware.eu>
2017-12-09 19:46:53 +0200

    FCM, need testing (?)

---
+++ app/Http/Controllers/FCMController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use LaravelFCM\Facades\FCM;
+use LaravelFCM\Message\OptionsBuilder;
+use LaravelFCM\Message\PayloadDataBuilder;
+use LaravelFCM\Message\PayloadNotificationBuilder;
+
+class FCMController extends Controller
+{
+    public function sentToOne($userToken, $Title, $Body, $Data)
+    {
+        $optionBuilder = new OptionsBuilder();
+        $optionBuilder->setTimeToLive(60 * 20);
+
+        $notificationBuilder = new PayloadNotificationBuilder($Title);
+        $notificationBuilder->setBody($Body)->setSound('default');
+
+        $dataBuilder = new PayloadDataBuilder();
+        $dataBuilder->addData($Data);
+
+        $option = $optionBuilder->build();
+        $notification = $notificationBuilder->build();
+        $data = $dataBuilder->build();
+
+        $downstreamResponse = FCM::sendTo($userToken, $option, $notification, $data);
+
+        //return Array - you must remove all this tokens in your database
+        $downstreamResponse->tokensToDelete();
+
+        //return Array (key : oldToken, value : new token - you must change the token in your database )
+        $downstreamResponse->tokensToModify();
+
+        //return Array - you should try to resend the message to the tokens in the array
+        $downstreamResponse->tokensToRetry();
+    }
+
+    public function sentToMultiple($userTokens, $Title, $Body, $Data)
+    {
+        $optionBuilder = new OptionsBuilder();
+        $optionBuilder->setTimeToLive(60 * 20);
+
+        $notificationBuilder = new PayloadNotificationBuilder($Title);
+        $notificationBuilder->setBody($Body)->setSound('default');
+
+        $dataBuilder = new PayloadDataBuilder();
+        $dataBuilder->addData($Data);
+
+        $option = $optionBuilder->build();
+        $notification = $notificationBuilder->build();
+        $data = $dataBuilder->build();
+
+        $downstreamResponse = FCM::sendTo($userTokens, $option, $notification, $data);
+
+        //return Array - you must remove all this tokens in your database
+        $downstreamResponse->tokensToDelete();
+
+        //return Array (key : oldToken, value : new token - you must change the token in your database )
+        $downstreamResponse->tokensToModify();
+
+        //return Array - you should try to resend the message to the tokens in the array
+        $downstreamResponse->tokensToRetry();
+    }
+}
