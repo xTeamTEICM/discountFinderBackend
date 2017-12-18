3d9f19a5b22df515389aefd281bc5cbdffe5a011
Jordan Kostelidis <JordanKostelidis@IEEE.org>
2017-12-18 13:54:01 +0200

    Accept ??? (Maybe)

--- routes/api.php
+++ routes/api.php
@@ -13,23 +13,20 @@ use Illuminate\Http\Request;
 |
 */
 
-// Auth Routes
+// Auth
 Route::post('register', 'Auth\RegisterController@register');
 Route::post('login', 'Auth\LoginController@login');
 Route::post('refresh', 'Auth\LoginController@refresh');
-Route::group(['middleware' => 'auth:api'], function(){
-
+Route::group(['middleware' => 'auth:api'], function () {
     Route::post('logout', 'Auth\LoginController@logout');
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
-
 });
 
-// Shops Routes
-Route::get('/shop','ShopController@list');
+// Shops
+Route::get('/shop', 'ShopController@list');
 Route::get('/shop/{id}', 'ShopController@get');
-
 Route::group(['middleware' => 'auth:api'], function () {
     Route::get('/user/shop', 'ShopController@myList');
     Route::get('/user/shop/{id}', 'ShopController@myGet');
@@ -39,42 +36,40 @@ Route::group(['middleware' => 'auth:api'], function () {
     Route::delete('/shop/{id}', 'ShopController@delete');
 });
 
-//Custom category controllers
-Route::get('/category','CategoryController@list');
-Route::get('/category/{id}','CategoryController@get');
-Route::post('/category','CategoryController@post');
-Route::put('/category','CategoryController@update');
-Route::delete('/category/{title}','CategoryController@remove');
+// Category
+Route::get('/category', 'CategoryController@list');
+Route::get('/category/{id}', 'CategoryController@get');
+Route::post('/category', 'CategoryController@post');
+Route::put('/category', 'CategoryController@update');
+Route::delete('/category/{title}', 'CategoryController@remove');
 
-//find discounts
-Route::middleware('auth:api')->post('/user/findDiscounts','FindDiscountsController@list');
-Route::middleware('auth:api')->post('/user/getTopList','FindDiscountsController@TopList');
+// Find Discounts
+Route::middleware('auth:api')->post('/user/findDiscounts', 'FindDiscountsController@list');
+Route::middleware('auth:api')->post('/user/getTopList', 'FindDiscountsController@TopList');
 
-//setDeviceToken
-Route::middleware('auth:api')->post('/user/deviceToken','DeviceTokenController@setDeviceToken');
+// FCM DeviceToken
+Route::middleware('auth:api')->post('/user/deviceToken', 'DeviceTokenController@setDeviceToken');
 
-// Requested Discount Routes
+// Requested Discounts
 Route::group(['middleware' => 'auth:api'], function () {
     Route::get('/requestedDiscount', 'RequestedDiscountController@list');
     Route::get('/requestedDiscount/{id}', 'RequestedDiscountController@get');
-    Route::post('/requestedDiscount','RequestedDiscountController@post');
-    Route::put('/requestedDiscount/{id}','RequestedDiscountController@put');
-    Route::delete('/requestedDiscount/{id}','RequestedDiscountController@delete');
+    Route::post('/requestedDiscount', 'RequestedDiscountController@post');
+    Route::put('/requestedDiscount/{id}', 'RequestedDiscountController@put');
+    Route::delete('/requestedDiscount/{id}', 'RequestedDiscountController@delete');
 });
 
-
 // Location
-Route::group(['middleware'=>'auth:api'],function () {
-    Route::put('/updateUserLocation','UpdateUserLocationController@update');
-    Route::get('/updateUserLocation','UpdateUserLocationController@list');
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::put('/updateUserLocation', 'UpdateUserLocationController@update');
+    Route::get('/updateUserLocation', 'UpdateUserLocationController@list');
 });
 
 //Discount Controller Routes
 Route::group(['middleware' => 'auth:api'], function () {
-Route::get('/discount','DiscountController@list');
-Route::get('/discount/{id}','DiscountController@get');
-Route::post('/discount','DiscountController@post');
-Route::put('/discount/{id}','DiscountController@put');
-Route::delete('discount/{id}','DiscountController@delete');
-
+    Route::get('/discount', 'DiscountController@list');
+    Route::get('/discount/{id}', 'DiscountController@get');
+    Route::post('/discount', 'DiscountController@post');
+    Route::put('/discount/{id}', 'DiscountController@put');
+    Route::delete('discount/{id}', 'DiscountController@delete');
 });
