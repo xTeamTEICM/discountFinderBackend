6463423ce8460d3e387984d49aca3ed5d071d519
Jordan Kostelidis <JordanKostelidis@IEEE.org>
2017-12-21 18:18:48 +0200

    Accept Image Controller #60

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -2,25 +2,47 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 
 class ImageController extends Controller
 {
     /**
-     * @param Request $request
+     * @param $title
+     * @param $data
+     * @param string $path
      * @return string
      */
-    public function save(Request $request)
+    public function save($title, $data, $path)
     {
+        if ($this->isBase64Image($data)) {
+            $file = $title . ".jpg";
+            file_put_contents($path . $file, base64_decode($data));
+            return $file;
+        } else {
+            return "Invalid Data";
+        }
 
-        $data = $this->validate($request, [
-            'title' => 'required|string',
-            'content' => 'required|string'
-        ]);
-
-        $path = "./../public/images/" . $data['title'] . ".jpg";
-        file_put_contents($path, base64_decode($data['content']));
-
-        return "/images/" . $data['title'] . ".jpg";
     }
+
+    public function delete($title, $path)
+    {
+        $file = $title . '.jpg';
+        if (file_exists($path . $file)) {
+            unlink($path . $file);
+            return "Deleted";
+        } else {
+            return "File not found";
+        }
+    }
+
+    private function isBase64Image($data)
+    {
+        $object = base64_decode($data);
+        if (!$img = @imagecreatefromstring($object)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+
 }
