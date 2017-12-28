19e2200d4a50b550a1b5a9ea5b07e301389f354c
Jordan Kostelidis <JordanKostelidis@IEEE.org>
2017-12-28 11:48:28 +0200

    Accept Sent notification on user's change location #67

--- app/Http/Controllers/UpdateUserLocationController.php
+++ app/Http/Controllers/UpdateUserLocationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use LaravelFCM\Message\InvalidOptionException;
 
 class UpdateUserLocationController extends Controller
 {
@@ -32,6 +33,43 @@ class UpdateUserLocationController extends Controller
         $user->latPos = $data['latPos'];
         $user->save();
         $user->push();
+
+        $findDiscounts = new FindDiscountsController();
+        $response = $findDiscounts->list(500, $request);
+
+        $count = 0;
+        foreach ($response as $sample) {
+            $count++;
+        }
+        try {
+            if ($count == 1) {
+                $fcm = new FCMController();
+
+                $fcm->sentToOne($user->deviceToken,
+                    'Βρήκαμε 1 προσφορά κοντά σας',
+                    'Δείτε την τώρα !',
+                    [
+                        'distance' => 500
+                    ],
+                    'discountNotification');
+
+            } elseif ($count >= 2) {
+
+                $fcm = new FCMController();
+
+                $fcm->sentToOne($user->deviceToken,
+                    'Βρήκαμε ' . $count . ' προσφορές κοντά σας',
+                    'Δείτε τις τώρα !',
+                    [
+                        'distance' => 500
+                    ],
+                    'discountsNotification');
+
+            }
+        } catch (InvalidOptionException $e) {
+            // ToDo : Handle Exception
+        }
+
 
         return response()->json([], 200);
 
