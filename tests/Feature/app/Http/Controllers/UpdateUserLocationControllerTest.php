ec95863a57a6e9cd61600e8db41ad351c13ec976
Jordan Kostelidis <JordanKostelidis@IEEE.org>
2017-12-29 14:45:22 +0200

    Accept Check if lat and log is valid before process the request #90

--- tests/Feature/app/Http/Controllers/UpdateUserLocationControllerTest.php
+++ tests/Feature/app/Http/Controllers/UpdateUserLocationControllerTest.php
@@ -25,7 +25,7 @@ class UpdateUserLocationControllerTest extends TestCase
         $response->assertStatus(422);
     }
 
-    public function testUpdateLocationSuccess()
+    public function testUpdateLocationInvalidLatLog()
     {
         $token = new AuthForTests();
         $token->generateToken($this);
@@ -33,7 +33,67 @@ class UpdateUserLocationControllerTest extends TestCase
 
         $response = $this->json('PUT', '/api/user/deviceLocation', [
             'logPos' => '123',
-            'latPos' => '321'
+            'latPos' => '456'
+        ], [
+            'Authorization' => $tokenKey
+        ]);
+        $response->assertStatus(422);
+    }
+
+    public function testUpdateLocationSuccess()
+    {
+        $token = new AuthForTests();
+        $token->generateToken($this);
+        $tokenKey = $token->getToken();
+
+        $response = $this->json('PUT', '/api/user/deviceLocation', [
+            'logPos' => '12.34',
+            'latPos' => '56.78'
+        ], [
+            'Authorization' => $tokenKey
+        ]);
+        $response->assertStatus(200);
+    }
+
+    public function testUpdateLocationSuccessVathylakkos()
+    {
+        $token = new AuthForTests();
+        $token->generateToken($this);
+        $tokenKey = $token->getToken();
+
+        $response = $this->json('PUT', '/api/user/deviceLocation', [
+            'logPos' => '22.708959',
+            'latPos' => '40.769614'
+        ], [
+            'Authorization' => $tokenKey
+        ]);
+        $response->assertStatus(200);
+    }
+
+    public function testUpdateLocationSuccessMammasPizza()
+    {
+        $token = new AuthForTests();
+        $token->generateToken($this);
+        $tokenKey = $token->getToken();
+
+        $response = $this->json('PUT', '/api/user/deviceLocation', [
+            'logPos' => '23.543211',
+            'latPos' => '41.082201'
+        ], [
+            'Authorization' => $tokenKey
+        ]);
+        $response->assertStatus(200);
+    }
+
+    public function testUpdateLocationSuccessTEICafe()
+    {
+        $token = new AuthForTests();
+        $token->generateToken($this);
+        $tokenKey = $token->getToken();
+
+        $response = $this->json('PUT', '/api/user/deviceLocation', [
+            'logPos' => '23.553907',
+            'latPos' => '41.074601'
         ], [
             'Authorization' => $tokenKey
         ]);
