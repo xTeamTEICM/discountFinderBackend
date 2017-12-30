ed1c77d8da0860af5e496531cc08a9a8843beb9b
Jordan Kostelidis <JordanKostelidis@IEEE.org>
2017-12-30 10:37:27 +0200

    Accept About api end point #102

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function get(Request $request)
+    {
+        return response()->json([
+            'Τίτλος Εφαρμογής' => 'Discount Finder API',
+            'Ομάδα' => 'xTEAMTEICM',
+            'Προγραμματιστές' => [
+                'Αβραάμ Κοβανίδης',
+                'Ιορδάνης Κωστελίδης',
+                'Κωνσταντίνος Μαντζαβέλας',
+                'Παύλος Κοκοζίδης',
+                'Νικόλαος Δελής',
+                'Μιγκέλ Δάιος Γαλάν',
+                'Αριστοτέλης Ποζίδης',
+                'Σωτήριος Κελεσίδης',
+                'Αρντίτ Ντόμι'
+            ],
+            'Υπέυθυνος Καθηγητής' => 'Νικόλαος Πεταλίδης',
+            'Έκδοση' => config("APP_VERSION", 'local')
+        ]);
+    }
+}
