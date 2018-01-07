32c473ecc8981ecc3b8a36529359aec5bf22c889
eLGreko21 <miguel.daios@hotmail.com>
2018-01-07 21:47:12 +0200

    added tests on Discount

--- config/app.php
+++ config/app.php
@@ -136,7 +136,7 @@ return [
     |
     */
 
-    'providers' => [
+    'providers' => array(
 
         /*
          * Laravel Framework Service Providers...
@@ -163,6 +163,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -180,7 +181,7 @@ return [
 
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
-    ],
+    ),
 
     /*
     |--------------------------------------------------------------------------
